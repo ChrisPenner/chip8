08f1722db790bc3b53a9c06558cfa939da2c8083
Chris Penner <christopher.penner@gmail.com>
2021-12-08 23:12:08 -0600

    Compiling with sdl2

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
+
+mod ops;
+
+fn main() {
+    ops::sayhi();
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let width = 800;
+    let height = 600;
+    let window = video_subsystem
+        .window("rust-sdl2 demo", width, height)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    let mut red = 0;
+    let mut green = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(red, green, i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::MouseMotion { x, y, .. } => {
+                    red = ((x as f32 / width as f32) * 255.0) as u8;
+                    green = ((y as f32 / height as f32) * 255.0) as u8;
+                }
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
---
+++ src/ops.rs
@@ -0,0 +1,3 @@
+pub fn sayhi() {
+    println!("hi");
+}
